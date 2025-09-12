8ab647bf93679cee4e395d1f3bf7098d728d1f66
jpnc1695 <joaopaulonunescosta@gmail.com>
2025-09-12 12:12:47 -0300

    Componente card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,39 +1,36 @@
 import './Card.css'
 import Form from 'react-bootstrap/Form';
 
-
 const Card = () => {
   return(
-
     <section className="card">
       <div>
         <h1>CHECK LIST </h1>
         <p>Implantação Loja</p>
         <p>2014 Interno</p>
       </div> 
-      <div>
+      <div className='form-radio'>
         <Form>
-        <Form.Check // prettier-ignore
-            type={'radio'}
-            id={`default`}
-            label={`Processo Pendente`}
-          />
-           <Form.Check // prettier-ignore
-            type={'radio'}
-            id={`default`}
-            label={`Processo Cumprido`}
-          />
-           <Form.Check // prettier-ignore
-            type={'radio'}
-            id={`default`}
-            label={`Não Aplica`}
-          />
+          <div className="radio-group">
+            <Form.Check
+              type={'radio'}
+              id={'pendente'}
+              label={'Processo Pendente'}
+            />
+            <Form.Check
+              type={'radio'}
+              id={'cumprido'}
+              label={'Processo Cumprido'}
+            />
+            <Form.Check
+              type={'radio'}
+              id={'nao-aplica'}
+              label={'Não Aplica'}
+            />
+          </div>
         </Form>
-        </div>  
-     
+      </div>  
     </section>
-
-    
   )
 }
 
