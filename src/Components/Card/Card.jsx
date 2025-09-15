ab9f3eb0b1a232119d9813171bb758929d39da4e
jpnc1695 <joaopaulonunescosta@gmail.com>
2025-09-15 14:52:12 -0300

    Desenvolvimento -- Ajuste nos checkbox

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,7 +1,21 @@
 import './Card.css';
 import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
+
 
 const Card = () => {
+
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleRadioChange = (value) => {
+    // Se clicar no mesmo radio que já está selecionado, desmarca
+    if (selectedOption === value) {
+      setSelectedOption(null);
+    } else {
+      setSelectedOption(value);
+    }
+  };
+
   return(
     <>
     <section className="card">
@@ -11,22 +25,31 @@ const Card = () => {
         <p>2014 Interno</p>
       </div> 
       <div className='form-radio'>
-        <Form>
+      <Form>
           <div className="radio-group">
             <Form.Check
               type={'radio'}
               id={'pendente'}
               label={'Processo Pendente'}
+              checked={selectedOption === 'pendente'}
+              onChange={() => handleRadioChange('pendente')}
+              key={1}
             />
             <Form.Check
               type={'radio'}
               id={'cumprido'}
               label={'Processo Cumprido'}
+              checked={selectedOption === 'cumprido'}
+              onChange={() => handleRadioChange('cumprido')}
+              key={2}
             />
             <Form.Check
               type={'radio'}
               id={'nao-aplica'}
               label={'Não Aplica'}
+              checked={selectedOption === 'nao-aplica'}
+              onChange={() => handleRadioChange('nao-aplica')}
+              key={3}
             />
           </div>
         </Form>
